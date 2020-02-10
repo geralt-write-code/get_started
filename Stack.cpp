9f133efb5b3d08a2f9c4f49a7d69681ae2eda166
geralt-write-code <923290181@qq.com>
2020-02-10 18:42:28 +0800

    add stack script by cpp

---
+++ Stack.cpp
@@ -0,0 +1,62 @@
+﻿// Stack.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<string>
+#include"Stack.h"
+//template<class t>
+//class Stack
+//{
+//public:
+//	Stack(unsigned int size=100)
+//	{
+//		this->size = size;
+//		data = new t[size];
+//		sp = 0;
+//	}
+//
+//	~Stack()
+//	{
+//		delete[]data;
+//
+//	}
+//
+//	void push(t value)
+//	{
+//		data[sp++] = value;
+//	}
+//	t pop()
+//	{
+//
+//		return data[--sp];
+//	}
+//
+//private:
+//	unsigned int size;
+//	unsigned int sp;
+//	t* data;
+//};
+//
+//
+//int main()
+//{
+//	Stack<int> intStack(100);
+//
+//	intStack.push(1);
+//	intStack.push(2);
+//	intStack.push(3);
+//
+//
+//	std::cout << intStack.pop() << "\n";
+//	std::cout << intStack.pop() << "\n";
+//	std::cout << intStack.pop() << "\n";
+//
+//	return 0;
+//}
+
+int main()
+{
+	cout << "hello world" << endl;
+}
+
+
